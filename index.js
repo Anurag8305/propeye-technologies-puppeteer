7e6e48a9b8968730c427bd891eb7106fad37c90b
Anurag Raj <anuragraj8305@gmail.com>
2023-04-30 23:09:56 +0530

    Input field auto data input done

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const puppeteer = require("puppeteer");
+const puppeteer=require("puppeteer");
 
-const url =
-	"https://in.indeed.com/jobs?q=software+developer&l=&from=searchOnHP&vjk=10399eacdabd0c6a";
-async function grab() {
-	const browser = await puppeteer.launch({ headless: false });
-	const page = await browser.newPage();
-	await page.goto(url);
-	const mainDiv = await page.evaluate(() => {
-		const element = Array.from(
-			document.querySelectorAll(
-				'#mosaic-provider-jobcards > ul > li:nth-child(1) > div'
-			)
-		);
-        let arr=[];
-        mainDiv.forEach((el)=>{
-            arr.push(el.innerHTML)
+
+(async()=>{
+    const browser=await puppeteer.launch({headless:false});
+    const page=await browser.newPage();
+    await page.goto("https://in.indeed.com/");
+    await page.type("#text-input-what","Software Developer",{delay:100});
+    await page.click("#jobsearch > button")
+    const grab=await page.evaluate(()=>{
+        const allData=Array.from(document.querySelectorAll("#mosaic-provider-jobcards > ul > li > div a"));
+        const company_name=document.querySelectorAll("#mosaic-provider-jobcards > ul > li > div > div > div > div.slider_item.css-kyg8or.eu4oa1w0 > div > table.jobCard_mainContent.big6_visualChanges > tbody > tr > td > div.heading6.company_location.tapItem-gutter.companyInfo > span");
+        const location=document.querySelectorAll("#mosaic-provider-jobcards > ul > li > div > div > div > div.slider_item.css-kyg8or.eu4oa1w0 > div > table.jobCard_mainContent.big6_visualChanges > tbody > tr > td > div.heading6.company_location.tapItem-gutter.companyInfo > div > div");
+        const date=document.querySelectorAll("#mosaic-provider-jobcards > ul > li > div > div > div > div.slider_item.css-kyg8or.eu4oa1w0 > div > table.jobCardShelfContainer.big6_visualChanges > tbody > tr.underShelfFooter > td > div > span > span");;
+        const desc=document.querySelectorAll("#mosaic-provider-jobcards > ul > li > div a");;
+        let arr=[]
+        const data=company_name.map((el)=>{
+            arr.push(el.innerText)
         })
-		return arr;
-	});
-	console.log(mainDiv);
-	await browser.close();
-}
-grab();
+        
+        return arr;
+    })
+
+    
+    console.log(grab);
+    //await browser.close();
+})()
+
