e802706a1e23b901b1e67d1e902c0db8923a2095
Anurag Raj <anuragraj8305@gmail.com>
2023-05-01 10:40:04 +0530

    Installed some other dependencies and made functional according to dynamic query for INDEED.com

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
-const puppeteer=require("puppeteer");
 
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
+// const { executablePath }:any = require("puppeteer")
+const url = "https://in.indeed.com/";
+const main = async () => {
+	const browser = await puppeteer.launch({ headless: false });
+	const page = await browser.newPage();
+	await page.goto(url);
+	await page.screenshot({ path: "bot.png", fullPage: true });
+	await page.type("#text-input-what", "Software Developer");
+	await page.click("[type=submit]");
+	await page.waitForTimeout(5000);
 
-(async()=>{
-    const browser=await puppeteer.launch({headless:false});
-    const page=await browser.newPage();
-    await page.goto("https://in.indeed.com/");
-    await page.type("#text-input-what","Software Developer",{delay:100});
-    await page.click("#jobsearch > button")
-    const grab=await page.evaluate(()=>{
-        const allData=Array.from(document.querySelectorAll("#mosaic-provider-jobcards > ul > li > div a"));
-        const company_name=document.querySelectorAll("#mosaic-provider-jobcards > ul > li > div > div > div > div.slider_item.css-kyg8or.eu4oa1w0 > div > table.jobCard_mainContent.big6_visualChanges > tbody > tr > td > div.heading6.company_location.tapItem-gutter.companyInfo > span");
-        const location=document.querySelectorAll("#mosaic-provider-jobcards > ul > li > div > div > div > div.slider_item.css-kyg8or.eu4oa1w0 > div > table.jobCard_mainContent.big6_visualChanges > tbody > tr > td > div.heading6.company_location.tapItem-gutter.companyInfo > div > div");
-        const date=document.querySelectorAll("#mosaic-provider-jobcards > ul > li > div > div > div > div.slider_item.css-kyg8or.eu4oa1w0 > div > table.jobCardShelfContainer.big6_visualChanges > tbody > tr.underShelfFooter > td > div > span > span");;
-        const desc=document.querySelectorAll("#mosaic-provider-jobcards > ul > li > div a");;
-        let arr=[]
-        const data=company_name.map((el)=>{
-            arr.push(el.innerText)
-        })
-        
-        return arr;
-    })
+	const grab = await page.evaluate(() => {
+		const allData = document.querySelectorAll(
+			"#mosaic-provider-jobcards > ul > li > div > a"
+		);
+		const heading = document.querySelectorAll(
+			"#mosaic-provider-jobcards > ul > li"
+		);
+		let arr = [];
+		heading.forEach((el) => {
+			let a = el.innerText;
+			a = a.trim().split("\n");
+            let temp1="",temp2="";
+            for(let i=4;i<a.length;i++){
+                temp1=temp1+a[i]+"."
+            }
+            for(let i=3;i<a.length;i++){
+                temp2=temp2+a[i]+"."
+            }
+			let obj = {
+				heading: a[0],
+				comapny_name: a[1] == "new" ? a[2] : a[1],
+				location: a[1] == "new" ? a[3] : a[2],
+				description: a[1] == "new" ? temp1:temp2
+			};
+			arr.push(obj);
+		});
 
-    
-    console.log(grab);
-    //await browser.close();
-})()
+		return arr;
+	});
 
+	console.log(grab);
+	await browser.close();
+};
+
+main();
